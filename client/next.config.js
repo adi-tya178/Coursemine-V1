8c009fbd4f4e5f3f89ad0a7300811d8d5089f95e
Aditya Harsh <pankajclg22@gmail.com>
2023-05-26 23:56:55 +0530

    Config final

--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,7 @@ const withAntdLess = require('next-plugin-antd-less');
 module.exports = withAntdLess({
     publicRuntimeConfig: {
         APP_NAME: 'Coursemine',
-        API: 'https://coursemine-api.herokuapp.com/api',
+        API: 'https://coursemine-v1-67t7.vercel.app/api',
         PRODUCTION: true,
         DOMAIN: 'https://coursemine-v1.vercel.app/',
         FB_APP_ID: 'JJSLKADFLKSAHFDSLKL'
